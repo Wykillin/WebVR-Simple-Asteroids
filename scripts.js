b086459f2be14c7cb1d49a7de236ae55b5128ef6
JonathanGrant <jonathanallengrant@gmail.com>
2016-11-05 16:56:47 -0700

    Got meteor respawning

--- scripts.js
+++ scripts.js
@@ -27,13 +27,56 @@ var Words = React.createClass({
   }
 })
 
-var AFrameScene = React.createClass({
+var Asteroid = React.createClass({
+  getInitialState() {
+    return {
+      x: this.props.x,
+      y: this.props.y,
+      z: this.props.z,
+      velocity: 0.5
+    }
+  },
+  componentDidMount: function() {
+    setTimeout(this.doPhysics, 10)
+  },
+  doPhysics: function() {
+    if(this.state.z >= 0) {
+      this.props.hitCamera()
+    } else {
+      this.setState({
+        z: this.state.z + this.state.velocity
+      })
+      setTimeout(this.doPhysics, 10)
+    }
+  },
   render: function() {
+    return (<a-sphere src='./meteortexture.jpg' position={this.state.x+' '+this.state.y+' '+this.state.z}></a-sphere>)
+  }
+})
+
+var AFrameScene = React.createClass({
+  getInitialState: function() {
+    var myAsteroid = <Asteroid key='a' x={0} y={0} z={-100} hitCamera={this.hitCamera} />
+    return {
+      asteroids: [myAsteroid],
+      numHit: 0,
+      numDestroyed: 0
+    }
+  },
+  hitCamera: function(asteroid) {
+    var newAsteroids = this.state.asteroids
+    newAsteroids.splice(asteroid, 1)
+    newAsteroids.push(<Asteroid key={Math.random() * 9999999} hitCamera={this.hitCamera} x={Math.random() * (10) - 5} y={Math.random() * (10) - 5} z={Math.random() * (-90)} />)
+    this.setState({asteroids: newAsteroids})
+  },
+  render: function() {
+    console.log(this.state)
+    var Asteroids = this.state.asteroids
     return (
-      <a-scene>
+      <a-scene >
         <Camera/>
         <Sky />
-        <Words />
+        {Asteroids}
       </a-scene>)
   }
 })
